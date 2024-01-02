f75c3b3b5bb8484c625ed477396501668f608827
Hwang <chrishuangyc@gmail.com>
2024-01-02 12:22:11 +0800

    Quiz code

---
+++ PGE QUIZ/Assets/Scripts/QuestionsAndAnswers.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionsAndAnswers : MonoBehaviour
+{
+    public string Question;
+    public string[] Answers;
+    public int CorrectAnswer;
+
+}
